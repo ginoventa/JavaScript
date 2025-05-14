ef6c968d192c2728a2b4d6d5b7aa020c8f928137
Ginoventa <gn.noventa@gmail.com>
2025-05-14 15:42:03 -0300

    From 0 to 6, like a dice.

--- randomGenerator/code.js
+++ randomGenerator/code.js
@@ -1,6 +1,6 @@
 // This function generates a random number between 0 and 100
 let number;
 document.getElementById("generator").onclick = function(){
-    number = Math.floor(Math.random()*10000); 
+    number = Math.floor(Math.random()*6); 
     document.getElementById("number").innerHTML = number;
 }
