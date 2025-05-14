a96b9753f6ccf63a59d2b937d72e5cda420743b0
Ginoventa <gn.noventa@gmail.com>
2025-05-14 19:53:36 -0300

    arrumando referenciação do js

---
+++ passwordGenerator/script.js
@@ -0,0 +1,44 @@
+function generateNumber(numberOfRolls) {
+    var randomNumber = []
+    var images = [];
+    var imageContainer = document.getElementById("images");
+
+    for(let i = 0; i < numberOfRolls; i++) {
+        randomNumber.push(Math.floor(Math.random() * 6) + 1);
+
+        switch (randomNumber[i]) {
+            case 1:
+                images.push("https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-one.svg ");
+                break;
+            case 2:
+                images.push("https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-two.svg ");
+                break;
+            case 3:
+                images.push("https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-three.svg ");
+                break;
+            case 4:
+                images.push("https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-four.svg ");
+                break;
+            case 5:
+                images.push("https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-five.svg ");
+                break;
+            case 6:
+                images.push("https://game-icons.net/icons/ffffff/000000/1x1/delapouite/dice-six-faces-six.svg ");
+                break;
+        }
+        var img = document.createElement("img");
+        img.src = images[i];
+        imageContainer.appendChild(img);
+    }
+    return randomNumber;
+}
+
+function rollDice(){
+    let numbersRolls = Number(document.getElementById("input").value);
+    let arrayOfNumbers = generateNumber(numbersRolls);
+    document.getElementById("number").textContent = "You rolled: " + arrayOfNumbers.join(", ");
+}
+
+document.getElementById("generator").onclick= function(){
+    rollDice();
+}
